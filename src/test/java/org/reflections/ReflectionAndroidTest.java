12746ce3509fa40cbb5bdd43cc4b70afde50cd0c
vineet <seattlevin1@gmail.com>
2021-11-20 22:01:51 -0800

    Fix PMD bugs

--- src/test/java/org/reflections/ReflectionAndroidTest.java
+++ src/test/java/org/reflections/ReflectionAndroidTest.java
@@ -2,8 +2,12 @@ package org.reflections;
 
 import org.junit.jupiter.api.Test;
 import org.reflections.scanners.Scanners;
+import org.reflections.serializers.JsonSerializer;
 import org.reflections.util.ConfigurationBuilder;
 import org.reflections.util.FilterBuilder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.reflections.ReflectionsTest.getUserDir;
 
 public class ReflectionAndroidTest {
 
@@ -13,12 +17,26 @@ public class ReflectionAndroidTest {
      */
     @Test
     public void testCollectAndroid() {
-        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
-                .forPackage("org.reflections")
-                .filterInputsBy(new FilterBuilder()
-                        .includePattern("org\\.reflections\\.TestModel\\$.*")
-                        .includePattern("Android\\.xml"))
-                .addScanners(Scanners.values());
-        Reflections reflections = new Reflections();
+        Reflections reflections = new Reflections(
+                new ConfigurationBuilder()
+                        .forPackage("org.reflections")
+                        .filterInputsBy(new FilterBuilder()
+                                .includePattern("org\\.reflections\\.TestModel\\$.*")
+                                .includePattern("Android*\\.xml"))
+                        .addScanners(Scanners.values()));
+
+        String targetDir = getUserDir() + "/target/test-classes";
+
+        // xml
+        reflections.save(targetDir + "/META-INF/reflections/saved-testModel-Android.xml");
+        assertEquals(
+                Reflections.collect("/META-INF/reflections/testModel-Android.xml", a -> true).getStore(),
+                Reflections.collect("/META-INF/reflections/saved-testModel-Android.xml", a -> true).getStore());
+
+        // json
+        reflections.save(targetDir + "/META-INF/reflections/saved-testModel-Android.json", new JsonSerializer());
+        assertEquals(
+                Reflections.collect("/META-INF/reflections/testModel-Android.json", a -> true).getStore(),
+                Reflections.collect("/META-INF/reflections/saved-testModel-Android.json", a -> true).getStore());
     }
 }
